bafb58cd8e0f91d1de7554c65f300f1582c50483
疯狂的狮子li <15040126243@163.com>
2021-12-30 17:06:21 +0800

    update 使用 jackson 全局处理时间

--- ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/web/controller/BaseController.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/web/controller/BaseController.java
@@ -26,23 +26,6 @@ public class BaseController
 {
 
     /**
-     * 将前台传递过来的日期格式的字符串，自动转化为Date类型
-     */
-    @InitBinder
-    public void initBinder(WebDataBinder binder)
-    {
-        // Date 类型转换
-        binder.registerCustomEditor(Date.class, new PropertyEditorSupport()
-        {
-            @Override
-            public void setAsText(String text)
-            {
-                setValue(DateUtils.parseDate(text));
-            }
-        });
-    }
-
-    /**
      * 设置请求分页数据
      */
     protected void startPage()
