bf019b405ea8fc0eaf56270ef62edb07172ccec6
疯狂的狮子li <15040126243@163.com>
2022-02-11 15:04:49 +0800

    add 增加 数据脱敏 功能

---
+++ ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysSensitiveServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ruoyi.system.service.impl;
+
+import com.ruoyi.common.core.service.SensitiveService;
+import com.ruoyi.common.satoken.utils.LoginHelper;
+import org.springframework.stereotype.Service;
+
+/**
+ * 脱敏服务
+ * 默认管理员不过滤
+ * 需自行根据业务重写实现
+ *
+ * @author Lion Li
+ */
+@Service
+public class SysSensitiveServiceImpl implements SensitiveService {
+
+    /**
+     * 是否脱敏
+     */
+    @Override
+    public boolean isSensitive() {
+        return LoginHelper.isAdmin();
+    }
+
+}
