24a8719d4c5c2b5d4ff1e0b3156b61c6cf397a20
疯狂的狮子li <15040126243@163.com>
2022-02-14 14:47:33 +0800

    update 优化xss注解处理逻辑

--- ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/xss/Xss.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/xss/Xss.java
@@ -16,9 +16,8 @@ import java.lang.annotation.Target;
 @Target(value = {ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.PARAMETER})
 @Constraint(validatedBy = {XssValidator.class})
 public @interface Xss {
-    String message()
 
-            default "不允许任何脚本运行";
+    String message() default "不允许任何脚本运行";
 
     Class<?>[] groups() default {};
 
