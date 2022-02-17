3f5834540bd93683ee92a54c957f7156a2d73b98
疯狂的狮子li <15040126243@163.com>
2022-02-17 19:25:59 +0800

    update 去除用户登录多余校验

--- ruoyi-auth/src/main/java/com/ruoyi/auth/service/SysLoginService.java
+++ ruoyi-auth/src/main/java/com/ruoyi/auth/service/SysLoginService.java
@@ -38,23 +38,6 @@ public class SysLoginService {
      * 登录
      */
     public LoginUser login(String username, String password) {
-        // 用户名或密码为空 错误
-        if (StringUtils.isAnyBlank(username, password)) {
-            recordLogininfor(username, Constants.LOGIN_FAIL, "用户/密码必须填写");
-            throw new ServiceException("用户/密码必须填写");
-        }
-        // 密码如果不在指定范围内 错误
-        if (password.length() < UserConstants.PASSWORD_MIN_LENGTH
-            || password.length() > UserConstants.PASSWORD_MAX_LENGTH) {
-            recordLogininfor(username, Constants.LOGIN_FAIL, "用户密码不在指定范围");
-            throw new ServiceException("用户密码不在指定范围");
-        }
-        // 用户名不在指定范围内 错误
-        if (username.length() < UserConstants.USERNAME_MIN_LENGTH
-            || username.length() > UserConstants.USERNAME_MAX_LENGTH) {
-            recordLogininfor(username, Constants.LOGIN_FAIL, "用户名不在指定范围");
-            throw new ServiceException("用户名不在指定范围");
-        }
         LoginUser userInfo;
         try {
             // 查询用户信息
