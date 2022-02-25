6678ec6b7bc5b20319e5a4bd39d3145cd3f0fc9c
疯狂的狮子li <15040126243@163.com>
2022-02-25 13:57:50 +0800

    update 调整dubbo自定义负载均衡配置注入 使用spring自动注入

--- ruoyi-common/ruoyi-common-loadbalancer/src/main/java/com/ruoyi/common/loadbalance/config/CustomLoadBalanceAutoConfiguration.java
+++ ruoyi-common/ruoyi-common-loadbalancer/src/main/java/com/ruoyi/common/loadbalance/config/CustomLoadBalanceAutoConfiguration.java
@@ -1,14 +1,12 @@
 package com.ruoyi.common.loadbalance.config;
 
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * 自定义负载均衡自动配置
  *
  * @author Lion Li
  */
-@PropertySource(value = "classpath:loadbalance.properties", encoding = "UTF-8")
 @LoadBalancerClients(defaultConfiguration = CustomLoadBalanceClientConfiguration.class)
 public class CustomLoadBalanceAutoConfiguration {
 
