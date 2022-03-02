0798f5340b3784bdf26fa4a02cdfcdff2b59adcd
疯狂的狮子Li <15040126243@163.com>
2022-03-03 01:57:10 +0800

    update 更新数据权限Service的使用说明

--- ruoyi-common/ruoyi-common-mybatis/src/main/java/com/ruoyi/common/mybatis/service/SysDataScopeService.java
+++ ruoyi-common/ruoyi-common-mybatis/src/main/java/com/ruoyi/common/mybatis/service/SysDataScopeService.java
@@ -5,7 +5,10 @@ import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Service;
 
 /**
- * 数据权限服务
+ * 数据权限 实现
+ * <p>
+ * 注意: 此Service内不允许调用标注`数据权限`注解的方法
+ * 例如: deptMapper.selectList 此 selectList 方法标注了`数据权限`注解 会出现循环解析的问题
  *
  * @author Lion Li
  */
--- ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/dubbo/RemoteDataScopeServiceImpl.java
+++ ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/dubbo/RemoteDataScopeServiceImpl.java
@@ -16,7 +16,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * 数据权限
+ * 数据权限 实现
+ * <p>
+ * 注意: 此Service内不允许调用标注`数据权限`注解的方法
+ * 例如: deptMapper.selectList 此 selectList 方法标注了`数据权限`注解 会出现循环解析的问题
  *
  * @author Lion Li
  */
