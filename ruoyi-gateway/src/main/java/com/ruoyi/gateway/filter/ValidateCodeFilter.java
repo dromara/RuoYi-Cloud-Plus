b83e0330a49d46fe7c22d5641857fc40262f9f54
疯狂的狮子li <15040126243@163.com>
2022-05-07 13:51:44 +0800

    fix 修复 网关未拦截 短信登录验证码

--- ruoyi-gateway/src/main/java/com/ruoyi/gateway/filter/ValidateCodeFilter.java
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/filter/ValidateCodeFilter.java
@@ -26,7 +26,7 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 @Component
 public class ValidateCodeFilter extends AbstractGatewayFilterFactory<Object> {
-    private final static String[] VALIDATE_URL = new String[]{"/auth/login", "/auth/register"};
+    private final static String[] VALIDATE_URL = new String[]{"/auth/login", "/auth/register", "/auth/smsLogin"};
 
     @Autowired
     private ValidateCodeService validateCodeService;
