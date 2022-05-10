f47f0808fddd9b2bb5787e98f201af49d5813beb
疯狂的狮子li <15040126243@163.com>
2022-05-10 14:48:48 +0800

    update 优化删除无用导包

--- ruoyi-modules/ruoyi-resource/src/main/java/com/ruoyi/resource/dubbo/RemoteSmsServiceImpl.java
+++ ruoyi-modules/ruoyi-resource/src/main/java/com/ruoyi/resource/dubbo/RemoteSmsServiceImpl.java
@@ -4,27 +4,15 @@ import cn.hutool.core.bean.BeanUtil;
 import com.ruoyi.common.core.domain.R;
 import com.ruoyi.common.core.exception.ServiceException;
 import com.ruoyi.common.core.utils.SpringUtils;
-import com.ruoyi.common.core.utils.StringUtils;
-import com.ruoyi.common.oss.entity.UploadResult;
-import com.ruoyi.common.oss.factory.OssFactory;
-import com.ruoyi.common.oss.service.IOssStrategy;
 import com.ruoyi.common.sms.config.properties.SmsProperties;
 import com.ruoyi.common.sms.core.SmsTemplate;
 import com.ruoyi.common.sms.entity.SmsResult;
-import com.ruoyi.resource.api.RemoteFileService;
 import com.ruoyi.resource.api.RemoteSmsService;
-import com.ruoyi.resource.api.domain.SysFile;
 import com.ruoyi.resource.api.domain.SysSms;
-import com.ruoyi.resource.domain.SysOss;
-import com.ruoyi.resource.mapper.SysOssMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
