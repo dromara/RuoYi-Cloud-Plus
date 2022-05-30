ada28231bd49ef0fadf57dc2e9d2d26f0e6158fc
疯狂的狮子li <15040126243@163.com>
2022-05-30 20:59:50 +0800

    add 增加 gateway 全局缓存过滤器 解决body不能重复读问题

---
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/filter/GlobalCacheRequestFilter.java
@@ -0,0 +1,39 @@
+package com.ruoyi.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 全局缓存获取body请求数据（解决流不能重复读取问题）
+ *
+ * @author Lion Li
+ */
+@Component
+public class GlobalCacheRequestFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // GET DELETE 不过滤
+        HttpMethod method = exchange.getRequest().getMethod();
+        if (method == null || method.matches("GET") || method.matches("DELETE")) {
+            return chain.filter(exchange);
+        }
+        return ServerWebExchangeUtils.cacheRequestBodyAndRequest(exchange, (serverHttpRequest) -> {
+            if (serverHttpRequest == exchange.getRequest()) {
+                return chain.filter(exchange);
+            }
+            return chain.filter(exchange.mutate().request(serverHttpRequest).build());
+        });
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
