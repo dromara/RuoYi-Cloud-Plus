f0fa36d005884fe982336783fd9dc40a99e47f4f
疯狂的狮子li <15040126243@163.com>
2022-05-30 21:30:39 +0800

    add 增加 全局日志 过滤器

---
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/filter/GlobalLogFilter.java
@@ -0,0 +1,120 @@
+package com.ruoyi.gateway.filter;
+
+import cn.hutool.core.map.MapUtil;
+import com.ruoyi.common.core.utils.JsonUtils;
+import com.ruoyi.common.core.utils.StringUtils;
+import com.ruoyi.gateway.config.properties.CustomGatewayProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashSet;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.CACHED_SERVER_HTTP_REQUEST_DECORATOR_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR;
+
+/**
+ * 全局日志过滤器
+ * <p>
+ * 用于打印请求执行参数与响应时间等等
+ *
+ * @author Lion Li
+ */
+@Slf4j
+@Component
+public class GlobalLogFilter implements GlobalFilter, Ordered {
+
+    @Autowired
+    private CustomGatewayProperties customGatewayProperties;
+
+    private static final String START_TIME = "startTime";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String path = getOriginalRequestUrl(exchange);
+        String url = request.getMethod().name() + " " + path;
+
+        if (!customGatewayProperties.getRequestLog()) {
+            return chain.filter(exchange);
+        }
+        // 打印请求参数
+        if (isJsonRequest(request)) {
+            ServerHttpRequestDecorator decorator = (ServerHttpRequestDecorator)
+                exchange.getAttributes().get(CACHED_SERVER_HTTP_REQUEST_DECORATOR_ATTR);
+            String jsonParam = resolveBodyFromRequest(decorator);
+            log.debug("[PLUS]开始请求 => URL[{}],参数类型[json],参数:[{}]", url, jsonParam);
+        } else {
+            MultiValueMap<String, String> parameterMap = request.getQueryParams();
+            if (MapUtil.isNotEmpty(parameterMap)) {
+                String parameters = JsonUtils.toJsonString(parameterMap);
+                log.debug("[PLUS]开始请求 => URL[{}],参数类型[param],参数:[{}]", url, parameters);
+            } else {
+                log.debug("[PLUS]开始请求 => URL[{}],无参数", url);
+            }
+        }
+
+        exchange.getAttributes().put(START_TIME, System.currentTimeMillis());
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            Long startTime = exchange.getAttribute(START_TIME);
+            if (startTime != null) {
+                long executeTime = (System.currentTimeMillis() - startTime);
+                log.debug("[PLUS]结束请求 => URL[{}],耗时:[{}]毫秒", url, executeTime);
+            }
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+
+    /**
+     * 判断本次请求的数据类型是否为json
+     *
+     * @param request request
+     * @return boolean
+     */
+    private boolean isJsonRequest(ServerHttpRequest request) {
+        MediaType contentType = request.getHeaders().getContentType();
+        if (contentType != null) {
+            return StringUtils.startsWithIgnoreCase(contentType.toString(), MediaType.APPLICATION_JSON_VALUE);
+        }
+        return false;
+    }
+
+    private String resolveBodyFromRequest(ServerHttpRequest serverHttpRequest) {
+        //获取请求体
+        Flux<DataBuffer> body = serverHttpRequest.getBody();
+        StringBuilder sb = new StringBuilder();
+        body.subscribe(buffer -> {
+            byte[] bytes = new byte[buffer.readableByteCount()];
+            buffer.read(bytes);
+            String bodyString = new String(bytes, StandardCharsets.UTF_8);
+            sb.append(bodyString);
+        });
+        return sb.toString();
+    }
+
+    public static String getOriginalRequestUrl(ServerWebExchange exchange) {
+        ServerHttpRequest request = exchange.getRequest();
+        LinkedHashSet<URI> uris = exchange.getRequiredAttribute(GATEWAY_ORIGINAL_REQUEST_URL_ATTR);
+        URI requestUri = uris.stream().findFirst().orElse(request.getURI());
+        return UriComponentsBuilder.fromPath(requestUri.getRawPath()).build().toUriString();
+    }
+
+}
