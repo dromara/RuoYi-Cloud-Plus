646423a3941433083d7088057f9d56c7805e1208
疯狂的狮子li <15040126243@163.com>
2022-06-15 17:02:13 +0800

    reset 回滚 InnerExclude 注解相关提交 错误的理解了功能

--- ruoyi-common/ruoyi-common-dubbo/src/main/java/cn/dev33/satoken/context/dubbo/filter/SaTokenDubboConsumerFilter.java
+++ ruoyi-common/ruoyi-common-dubbo/src/main/java/cn/dev33/satoken/context/dubbo/filter/SaTokenDubboConsumerFilter.java
@@ -6,15 +6,10 @@ import cn.dev33.satoken.id.SaIdUtil;
 import cn.dev33.satoken.spring.SaBeanInject;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaTokenConsts;
-import cn.hutool.core.annotation.AnnotationUtil;
-import com.ruoyi.common.core.annotation.InnerExclude;
 import com.ruoyi.common.core.utils.SpringUtils;
-import lombok.SneakyThrows;
 import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.rpc.*;
-
-import java.lang.reflect.Method;
 
 /**
  *
@@ -28,7 +23,6 @@ import java.lang.reflect.Method;
 @Activate(group = {CommonConstants.CONSUMER}, order = Integer.MIN_VALUE)
 public class SaTokenDubboConsumerFilter implements Filter {
 
-    @SneakyThrows(NoSuchMethodException.class)
 	@Override
 	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         // 强制初始化 Sa-Token 相关配置 解决内网鉴权元数据加载报错问题
@@ -36,16 +30,7 @@ public class SaTokenDubboConsumerFilter implements Filter {
 
         // 追加 Id-Token 参数
 		if(SaManager.getConfig().getCheckIdToken()) {
-            Class<?> clazz = invoker.getInterface();
-            Method method = clazz.getMethod(invocation.getMethodName(), invocation.getParameterTypes());
-            // 检查是否有内网鉴权排除注解
-            if (AnnotationUtil.hasAnnotation(clazz, InnerExclude.class)
-                || AnnotationUtil.hasAnnotation(method, InnerExclude.class)) {
-                // 不传递 Id-Token
-            } else {
-                RpcContext.getServiceContext().setAttachment(SaIdUtil.ID_TOKEN, SaIdUtil.getToken());
-            }
-
+            RpcContext.getServiceContext().setAttachment(SaIdUtil.ID_TOKEN, SaIdUtil.getToken());
 		}
 
 		// 1. 调用前，向下传递会话Token
