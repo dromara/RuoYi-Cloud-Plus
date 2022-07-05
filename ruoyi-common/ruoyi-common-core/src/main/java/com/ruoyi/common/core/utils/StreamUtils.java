f1d102916a07c94f9080e0b04f6739c6857a2242
疯狂的狮子li <15040126243@163.com>
2022-07-05 11:04:34 +0800

    add 增加 StreamUtils 流工具 简化 stream 流操纵

---
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/utils/StreamUtils.java
@@ -0,0 +1,42 @@
+package com.ruoyi.common.core.utils;
+
+import cn.hutool.core.collection.CollStreamUtil;
+import cn.hutool.core.collection.CollUtil;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * stream 流工具类
+ *
+ * @author Lion Li
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class StreamUtils extends CollStreamUtil {
+
+    public static <E> List<E> filter(Collection<E> collection, Predicate<E> function) {
+        if (CollUtil.isEmpty(collection)) {
+            return Collections.emptyList();
+        }
+        return collection.stream().filter(function).collect(Collectors.toList());
+    }
+
+    public static <E> String join(Collection<E> collection,  Function<E, String> function) {
+        return join(collection, function, ",");
+    }
+
+    public static <E> String join(Collection<E> collection,  Function<E, String> function, CharSequence delimiter) {
+        if (CollUtil.isEmpty(collection)) {
+            return StringUtils.EMPTY;
+        }
+        return collection.stream().map(function).filter(Objects::nonNull).collect(Collectors.joining(delimiter));
+    }
+
+    public static <E> List<E> sorted(Collection<E> collection, Comparator<E> comparing) {
+        return collection.stream().sorted(comparing).collect(Collectors.toList());
+    }
+}
