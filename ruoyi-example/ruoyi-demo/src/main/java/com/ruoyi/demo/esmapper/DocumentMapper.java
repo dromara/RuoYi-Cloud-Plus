4c5ae5f4f09953326ed54e0862c76aa72dbdddc7
疯狂的狮子li <15040126243@163.com>
2022-08-03 15:44:28 +0800

    add 增加 搜索引擎 crud 演示案例

---
+++ ruoyi-example/ruoyi-demo/src/main/java/com/ruoyi/demo/esmapper/DocumentMapper.java
@@ -0,0 +1,7 @@
+package com.ruoyi.demo.esmapper;
+
+import cn.easyes.core.conditions.interfaces.BaseEsMapper;
+import com.ruoyi.demo.domain.Document;
+
+public interface DocumentMapper extends BaseEsMapper<Document> {
+}
