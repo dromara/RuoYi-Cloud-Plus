21c3635a04e8ae4802b6541169b88296d08098ff
疯狂的狮子Li <15040126243@163.com>
2022-08-07 13:23:20 +0800

    add 增加 全局跨域过滤器 处理跨域请求 适配移动端访问

---
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/filter/GlobalCorsFilter.java
@@ -0,0 +1,58 @@
+package com.ruoyi.gateway.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+
+/**
+ * 跨域配置
+ *
+ * @author Lion Li
+ */
+@Component
+public class GlobalCorsFilter implements WebFilter, Ordered {
+
+    /**
+     * 这里为支持的请求头，如果有自定义的header字段请自己添加
+     */
+    private static final String ALLOWED_HEADERS = "X-Requested-With, Content-Type, Authorization, credential, X-XSRF-TOKEN, token, Admin-Token, App-Token";
+    private static final String ALLOWED_METHODS = "GET,POST,PUT,DELETE,OPTIONS,HEAD";
+    private static final String ALLOWED_ORIGIN = "*";
+    private static final String ALLOWED_EXPOSE = "*";
+    private static final String MAX_AGE = "18000L";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        if (CorsUtils.isCorsRequest(request)) {
+            ServerHttpResponse response = exchange.getResponse();
+            HttpHeaders headers = response.getHeaders();
+            headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+            headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+            headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+            headers.add("Access-Control-Expose-Headers", ALLOWED_EXPOSE);
+            headers.add("Access-Control-Max-Age", MAX_AGE);
+            headers.add("Access-Control-Allow-Credentials", "true");
+            if (request.getMethod() == HttpMethod.OPTIONS) {
+                response.setStatusCode(HttpStatus.OK);
+                return Mono.empty();
+            }
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
