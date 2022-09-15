bd0cce5540e06b5012baa7ef8d3628f068a8ed5f
疯狂的狮子li <15040126243@163.com>
2022-09-15 09:50:23 +0800

    fix 修复 不同网段因reset请求头导致下载导出跨域问题

--- ruoyi-modules/ruoyi-gen/src/main/java/com/ruoyi/gen/controller/GenController.java
+++ ruoyi-modules/ruoyi-gen/src/main/java/com/ruoyi/gen/controller/GenController.java
@@ -195,7 +195,6 @@ public class GenController extends BaseController {
      * 生成zip文件
      */
     private void genCode(HttpServletResponse response, byte[] data) throws IOException {
-        response.reset();
         response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Expose-Headers", "Content-Disposition");
         response.setHeader("Content-Disposition", "attachment; filename=\"ruoyi.zip\"");
