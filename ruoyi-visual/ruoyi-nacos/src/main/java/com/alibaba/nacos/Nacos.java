a97bd2b73ddc73c8b906460f50576b9e5782b8d4
疯狂的狮子li <15040126243@163.com>
2022-10-19 13:02:36 +0800

    update 优化 nacos 集群模式搭建 关于 nacos.home 注释说明

--- ruoyi-visual/ruoyi-nacos/src/main/java/com/alibaba/nacos/Nacos.java
+++ ruoyi-visual/ruoyi-nacos/src/main/java/com/alibaba/nacos/Nacos.java
@@ -35,7 +35,8 @@ public class Nacos {
         // true 单机模式 false 为集群模式 集群模式需搭配 cluster.conf 使用 使用方法请查看文档
         System.setProperty("nacos.standalone", "true");
         System.setProperty("server.tomcat.accesslog.enabled", "false");
-        // 本地集群搭建使用 在此目录下创建 conf/cluster.conf 文件用于编写集群ip端口
+        // 本地集群搭建使用 分别在所有 nacos 目录下创建 conf/cluster.conf 文件用于编写集群ip端口
+        // 注意 如果本地启动多个 nacos 此目录不能相同 例如 nacos1 nacos2 nacos3 对应三个nacos服务
         // System.setProperty("nacos.home", "D:/nacos");
         SpringApplication.run(Nacos.class, args);
     }
