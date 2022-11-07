d841ecee9cd792d7b9b644ff8b5508f5f2c64326
疯狂的狮子li <15040126243@163.com>
2022-11-07 15:42:29 +0800

    fix 修复 dubbo 使用 tri 协议 header 请求头变为小写导致无法获取参数问题

--- ruoyi-common/ruoyi-common-dubbo/src/main/java/cn/dev33/satoken/context/dubbo/filter/SaTokenDubboProviderFilter.java
+++ ruoyi-common/ruoyi-common-dubbo/src/main/java/cn/dev33/satoken/context/dubbo/filter/SaTokenDubboProviderFilter.java
@@ -28,6 +28,10 @@ public class SaTokenDubboProviderFilter implements Filter {
         // RPC 调用鉴权
 		if(SaManager.getConfig().getCheckSameToken()) {
 			String idToken = invocation.getAttachment(SaSameUtil.SAME_TOKEN);
+            // dubbo部分协议会将参数变为小写，详细参考：https://gitee.com/dromara/sa-token/issues/I4WXQG
+            if(idToken == null) {
+                idToken = invocation.getAttachment(SaSameUtil.SAME_TOKEN.toLowerCase());
+            }
             SaSameUtil.checkToken(idToken);
 		}
 
