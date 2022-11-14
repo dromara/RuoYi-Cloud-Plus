b190384b00ecd6f481333bb8b2b2d5967f222c18
疯狂的狮子li <15040126243@163.com>
2022-11-14 12:04:44 +0800

    add 新增 使用 mica-metrics 为 undertow 提供健康检查

---
+++ ruoyi-common/ruoyi-common-web/src/main/java/net/dreamlu/mica/metrics/druid/DruidMetricsConfiguration.java
@@ -0,0 +1,11 @@
+package net.dreamlu.mica.metrics.druid;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+/**
+ * 修复 mica 自带的druid类导致报错问题
+ */
+@AutoConfiguration
+public class DruidMetricsConfiguration {
+
+}
