714f051cb3952c8303fd282780b5013dbd11f8bb
疯狂的狮子li <15040126243@163.com>
2023-02-15 09:41:10 +0800

    fix 修复 !pr70 相关问题

--- ruoyi-common/ruoyi-common-encrypt/src/main/java/com/ruoyi/common/encrypt/config/EncryptorAutoConfiguration.java
+++ ruoyi-common/ruoyi-common-encrypt/src/main/java/com/ruoyi/common/encrypt/config/EncryptorAutoConfiguration.java
@@ -7,6 +7,7 @@ import com.ruoyi.common.encrypt.properties.EncryptorProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 /**
