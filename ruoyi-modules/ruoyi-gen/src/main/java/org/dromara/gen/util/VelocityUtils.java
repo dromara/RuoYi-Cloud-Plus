c1d824c7bec864fc9ad58b48b518ae38a9797371
疯狂的狮子Li <15040126243@163.com>
2023-05-31 22:54:12 +0800

    update 优化 代码生成器

--- ruoyi-modules/ruoyi-gen/src/main/java/org/dromara/gen/util/VelocityUtils.java
+++ ruoyi-modules/ruoyi-gen/src/main/java/org/dromara/gen/util/VelocityUtils.java
@@ -109,7 +109,7 @@ public class VelocityUtils {
      * @return 模板列表
      */
     public static List<String> getTemplateList(String tplCategory) {
-        List<String> templates = new ArrayList<String>();
+        List<String> templates = new ArrayList<>();
         templates.add("vm/java/domain.java.vm");
         templates.add("vm/java/vo.java.vm");
         templates.add("vm/java/bo.java.vm");
