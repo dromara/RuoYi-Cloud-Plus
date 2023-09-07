7ac75ba36ae7ae024e5b94d3d4ba5ca11d22c309
疯狂的狮子Li <15040126243@163.com>
2023-09-07 11:54:37 +0000

    update 优化 携带 clientid 跨域问题
    Signed-off-by: 疯狂的狮子Li <15040126243@163.com>

--- ruoyi-gateway/src/main/java/org/dromara/gateway/filter/GlobalCorsFilter.java
+++ ruoyi-gateway/src/main/java/org/dromara/gateway/filter/GlobalCorsFilter.java
@@ -25,7 +25,7 @@ public class GlobalCorsFilter implements WebFilter, Ordered {
     /**
      * 这里为支持的请求头，如果有自定义的header字段请自己添加
      */
-    private static final String ALLOWED_HEADERS = "X-Requested-With, Content-Language, Content-Type, Authorization, credential, X-XSRF-TOKEN, isToken, token, Admin-Token, App-Token";
+    private static final String ALLOWED_HEADERS = "X-Requested-With, Content-Language, Content-Type, Authorization, clientid, credential, X-XSRF-TOKEN, isToken, token, Admin-Token, App-Token";
     private static final String ALLOWED_METHODS = "GET,POST,PUT,DELETE,OPTIONS,HEAD";
     private static final String ALLOWED_ORIGIN = "*";
     private static final String ALLOWED_EXPOSE = "*";
