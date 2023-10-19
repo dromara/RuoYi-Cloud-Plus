b0dac358f784479b33a28e38f87bae9cfd4d8c7c
疯狂的狮子Li <15040126243@163.com>
2023-10-19 14:19:52 +0800

    update 优化 移除不合理的方法 携带附件的邮件建议直接集成插件发送

--- ruoyi-api/ruoyi-api-resource/src/main/java/org/dromara/resource/api/RemoteMailService.java
+++ ruoyi-api/ruoyi-api-resource/src/main/java/org/dromara/resource/api/RemoteMailService.java
@@ -2,9 +2,6 @@ package org.dromara.resource.api;
 
 import org.dromara.common.core.exception.ServiceException;
 
-import java.io.File;
-import java.util.List;
-
 /**
  * 邮件服务
  *
@@ -20,15 +17,5 @@ public interface RemoteMailService {
      * @param text    内容
      */
     void send(String to, String subject, String text) throws ServiceException;
-
-    /**
-     * 发送邮件带附件
-     *
-     * @param to       接收人
-     * @param subject  标题
-     * @param text     内容
-     * @param fileList 附件
-     */
-    void sendWithAttachment(String to, String subject, String text, List<File> fileList) throws ServiceException;
 
 }
--- ruoyi-modules/ruoyi-resource/src/main/java/org/dromara/resource/dubbo/RemoteMailServiceImpl.java
+++ ruoyi-modules/ruoyi-resource/src/main/java/org/dromara/resource/dubbo/RemoteMailServiceImpl.java
@@ -1,16 +1,12 @@
 package org.dromara.resource.dubbo;
 
-import cn.hutool.core.util.ArrayUtil;
-import org.dromara.common.core.exception.ServiceException;
-import org.dromara.common.mail.utils.MailUtils;
-import org.dromara.resource.api.RemoteMailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.dromara.common.core.exception.ServiceException;
+import org.dromara.common.mail.utils.MailUtils;
+import org.dromara.resource.api.RemoteMailService;
 import org.springframework.stereotype.Service;
-
-import java.io.File;
-import java.util.List;
 
 /**
  * 邮件服务
@@ -32,18 +28,6 @@ public class RemoteMailServiceImpl implements RemoteMailService {
      */
     public void send(String to, String subject, String text) throws ServiceException {
         MailUtils.sendText(to, subject, text);
-    }
-
-    /**
-     * 发送邮件带附件
-     *
-     * @param to       接收人
-     * @param subject  标题
-     * @param text     内容
-     * @param fileList 附件
-     */
-    public void sendWithAttachment(String to, String subject, String text, List<File> fileList) throws ServiceException {
-        MailUtils.sendText(to, subject, text, ArrayUtil.toArray(fileList, File.class));
     }
 
 }
