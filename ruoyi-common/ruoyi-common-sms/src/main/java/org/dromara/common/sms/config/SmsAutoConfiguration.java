6d2fb2c32b758f9a82396cbed501fc82ea8aaa13
疯狂的狮子Li <15040126243@163.com>
2023-12-07 17:34:45 +0800

    update 优化 sms组件 预留自动配置类

--- ruoyi-common/ruoyi-common-sms/src/main/java/org/dromara/common/sms/config/SmsAutoConfiguration.java
+++ ruoyi-common/ruoyi-common-sms/src/main/java/org/dromara/common/sms/config/SmsAutoConfiguration.java
@@ -1,15 +1,14 @@
-package com.ruoyi.common.sms.config;
+package org.dromara.common.sms.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 
 /**
- * 短信配置类
+ * 短信配置类(暂时没用 预留扩展)
  *
  * @author Lion Li
  * @version 4.2.0
  */
 @AutoConfiguration
-//@EnableConfigurationProperties(SmsProperties.class)
 public class SmsAutoConfiguration {
 
 }
