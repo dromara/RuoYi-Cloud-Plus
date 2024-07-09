5c072f8eb1b05b54d6ef682c3eb42b076d861885
疯狂的狮子Li <15040126243@163.com>
2024-07-09 14:49:40 +0800

    update 优化 kafka stream 默认开启避免小白搞不懂

--- ruoyi-example/ruoyi-test-mq/src/main/java/org/dromara/stream/config/KafkaStreamsConfig.java
+++ ruoyi-example/ruoyi-test-mq/src/main/java/org/dromara/stream/config/KafkaStreamsConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 /**
  * kafka stream 配置
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
  * @author LionLi
  */
 @Configuration
+@EnableKafkaStreams
 public class KafkaStreamsConfig {
 
     @Bean
